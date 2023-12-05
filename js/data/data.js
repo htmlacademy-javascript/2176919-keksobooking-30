f2f8634debf064c547cbbad23bb4d1360db8ffee
Floias <irina.butcher@gmail.com>
2023-12-05 21:22:54 +0500

    ДЗ первой недели

---
+++ js/data/data.js
@@ -0,0 +1,33 @@
+
+const MARKS = 10;
+
+const TYPES_HOUSES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const CONTROL_TIMESTAMPS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FACILITIES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const URLS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+export {MARKS, TYPES_HOUSES, CONTROL_TIMESTAMPS, FACILITIES, URLS};
