2fab26b64bad46b2a32df6f1735b7dfdf950f2ac
Floias <irina.butcher@gmail.com>
2023-12-15 17:32:48 +0500

    убирает лишнее

--- js/main.js
+++ js/main.js
@@ -1,11 +1,2 @@
 import './modules/validation.js';
 import './modules/map.js';
-import { returnAdvertisements } from './modules/advertisements.js';
-import { MARKS } from './data/data.js';
-import { creatingSimilarAds } from './modules/creating-similar-ads.js';
-
-const data = returnAdvertisements(MARKS);//генерирует 10 объектов объявлений
-
-creatingSimilarAds(data[3]);//показывает одно объявление
-console.log(data)
-//offer.title и location.lat location.lng
